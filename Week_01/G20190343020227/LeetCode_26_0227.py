65336b210d7a673caac97a7807e15c5e15c66f92
int86 <273206894@qq.com>
2019-12-12 00:34:12 +0800

    两数之和，删除排序数组的重复项，加一

---
+++ Week_01/G20190343020227/LeetCode_26_0227.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0: return 0
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i+1
