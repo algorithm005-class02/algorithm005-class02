73ed848522bfdff0f0a67ab95f166fa3d0f85ba1
朱晨阳 <pozhenzi9010@live.com>
2019-12-12 23:58:49 +0800

    tj

---
+++ Week_01/G20190343020299/LeetCode_26_0299.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        // 双指针法
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 1, 1, 2, 2, 3, 4, 5, 6, 6, 7 };
+        int res = new Solution().removeDuplicates(nums);
+        String numsStr = Arrays.stream(nums).boxed().map(x -> x.toString()).collect(Collectors.joining(","));
+
+        System.out.println(res);
+        System.out.println(numsStr);
+    }
+}
