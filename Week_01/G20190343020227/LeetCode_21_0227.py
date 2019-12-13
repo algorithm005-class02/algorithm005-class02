8159e940052ff01503ee2fc812edf6cb5b19e6ed
max_chen <273206894@qq.com>
2019-12-13 16:15:40 +0800

    修改合并两个有序连表

--- Week_01/G20190343020227/LeetCode_21_0227.py
+++ Week_01/G20190343020227/LeetCode_21_0227.py
@@ -3,8 +3,12 @@
 class Solution:
     def mergeTwoLists(self, l1, l2):
         if l1 and l2:
-            if l1.val > l2.val:
-                l1,l2 = l2,l1
-            l1.next = self.mergeTwoLists(l1.next,l2)
+            if l1.val < l2.val:
+                l1.next = self.mergeTwoLists(l1.next,l2)
+                return l1
+            else:
+                l2.next = self.mergeTwoLists(l1,l2.next)
+                return l2
 
         return l1 or l2
+
