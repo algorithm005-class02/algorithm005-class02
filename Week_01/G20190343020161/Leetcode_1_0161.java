deaa5f0a07a458cd83a03a8692a77934b5077b30
bigWill <870993074@qq.com>
2019-12-13 18:34:53 +0800

    Two sum

--- Week_01/G20190343020161/Leetcode_1_0161.java
+++ Week_01/G20190343020161/Leetcode_1_0161.java
@@ -31,7 +31,7 @@ class Solution {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i] - target)) {
+            if (map.containsKey(target - nums[i])) {
                 result[1] = i;
                 result[0] = map.get(target - nums[i]);
                 return result;
