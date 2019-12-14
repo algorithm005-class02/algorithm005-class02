7609d39469c5906499d088280d7f779b0329e032
任亮 <ren@renliang.local>
2019-12-14 20:44:20 +0800

    修改15题文件

--- Week_01/G20190343020185/LeetCode_15_0185.js
+++ Week_01/G20190343020185/LeetCode_15_0185.js
@@ -5,24 +5,23 @@
  */
 var threeSum1 = function(nums) {
     nums.sort((function(a,b){return a-b}));
-    var res = [],d=[],tmp = 9999;
-    console.log(nums)
-    for(var i =  0; i< nums.length;i++){
-        if(nums[i]>0) break;
+    var res = [],d = [],tmp = 9999;
+    for(var i = 0; i < nums.length; i++){
+        if(nums[i] > 0) break;
         if(tmp == nums[i]) continue;
         tmp = nums[i];
-        var j=i+1,k=nums.length-1;
-        while(j<k){
-            var tmpsum = nums[i] + nums[j]+nums[k];
-            var tmpleft = nums[j],tmpright=nums[k];
+        var j = i + 1, k = nums.length - 1;
+        while(j < k){
+            var tmpsum = nums[i] + nums[j] + nums[k];
+            var tmpleft = nums[j],tmpright = nums[k];
             if(tmpsum < 0) j++;
             else if(tmpsum > 0) k--;
             else{
                 d.push(nums[i],nums[j],nums[k]);
                 res.push(d);
                 d=[];
-                while(nums[j]-tmpleft == 0) j++;
-                while(nums[k]-tmpright == 0) k--;
+                while(nums[j] - tmpleft == 0) j++;
+                while(nums[k] - tmpright == 0) k--;
             }
         }
     }
