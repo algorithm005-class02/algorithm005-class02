09604c936096ee032fd31620118c8cf9ae9f9782
大漠胡杨 <169163301@qq.com>
2019-12-14 16:28:02 +0800

    Update LeetCode_66_0043.c

--- Week_01/G20190343020043/LeetCode_66_0043.c
+++ Week_01/G20190343020043/LeetCode_66_0043.c
@@ -1,31 +1,18 @@
 int* plusOne(int* digits, int digitsSize, int* returnSize){
-    int* res = NULL;
-    *returnSize = 0;
-    int m = -1;
     for (int i = digitsSize - 1; i >= 0; i--) {
         if (digits[i] < 9) {
-            m = i;
-            break;
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+        } else {
+            digits[i] = 0;
         }
     }
-    if (m == -1) {
-        res = malloc((digitsSize + 1) * sizeof(int));
-        *returnSize = digitsSize + 1;
-        res[0] = 1;
-        for (int i = 1; i <= digitsSize; i++) {
-            res[i] = 0;
-        }
-        return res;
-    } else {
-        res = malloc(digitsSize * sizeof(int));
-        *returnSize = digitsSize;
-        res[m] = digits[m] + 1;
-        for ( int i = 0; i < m; i++) {
-            res[i] = digits[i];
-        }
-        for ( int i = m + 1; i < digitsSize; i++) {
-            res[i] = 0;
-        }
-        return res;
+    int* res = malloc((digitsSize + 1) * sizeof(int));
+    *returnSize = digitsSize + 1;
+    res[0] = 1;
+    for (int i = 1; i <= digitsSize; i++) {
+        res[i] = 0;
     }
+    return res;
 }
