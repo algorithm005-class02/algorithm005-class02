07eb6f3527fe8d5989d5b32bdebb96978f8796f3
任亮 <ren@renliang.local>
2019-12-14 20:56:30 +0800

    按照建议修改LeetCode283题目 移动0 的算法实现

--- Week_01/G20190343020185/LeetCode_283_0185.js
+++ Week_01/G20190343020185/LeetCode_283_0185.js
@@ -3,28 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+	//指针
     var s = 0;
-    var t = 0;
+	//交换
+	var tmp = 0;
+	//处理
     for (var i=0; i<nums.length; i++) {
-        if(nums[i] == 0){
-            t++;
+        if(nums[i] != 0){
+			tmp = nums[s];
+			nums[s++] = nums[i];
+			nums[i] = tmp;
         }
-        //指针
-        s = s>i?s:i;
-        s = s>nums.length-1?nums.length-1:s;
-        for(var j = s; j<nums.length;j++){
-            if(nums[j] != 0){
-                s=j;
-                break;
-            }
-        }
-        console.log(i+','+s);
-        nums[i] = nums[s++];
     }
-    for(var k = 0;k<t;k++){
-        nums[nums.length-k-1] = 0;
-    }
-    return nums
+    return nums;
 };
 
 export default moveZeroes;
