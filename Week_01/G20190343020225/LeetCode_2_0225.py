ea64ea1f1554f9b35b9462c64f93952a59c715d0
Der Erfolg <baoqing1995@foxmail.com>
2019-12-14 11:29:49 +0800

    sfdf

--- Week_01/G20190343020225/LeetCode_2_0225.py
+++ Week_01/G20190343020225/LeetCode_2_0225.py
@@ -1,16 +1,32 @@
-# hashmap
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    hash_map = {}
-    for i,num in enumerate(nums):
-        another_num = target - num
-        if another_num in hash_map:
-            return [hash_map[another_num], i]
-        hash_map[num] = i
-    return None
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
 
-# 暴力法,循环遍历nums[i]和nums[j]
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    for i in range(len(nums)-1):
-        for j in range(i,len(nums)):
-            if nums[i] + nums[j] == target and i!=j:
-                return [i,j]
+# @lc code=start
+class Solution:
+
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     hash_map = {}
+    #     for i,num in enumerate(nums):
+    #         another_num = target - num
+    #         if another_num in hash_map:
+    #             return [hash_map[another_num], i]
+    #         hash_map[num] = i
+        # return None
+
+
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:    
+        # for i in range(len(nums)-1):
+        #     for j in range(i,len(nums)):
+        #         if nums[i] + nums[j] == target and i!=j:
+        #             return [i,j]
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]: 
+        for i in range(len(nums)):
+            if target - nums[i] in nums and nums.index(target - nums[i]) != i:
+                return [i,nums.index(target-nums[i])]
+
+# @lc code=end
+
--- Week_01/G20190343020225/test.py
+++
@@ -1,2 +0,0 @@
-def abc():
-  pass
