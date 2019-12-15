1c0b666afa88af7984a486bdd03cca447b836280
togodevlop <losenve@126.com>
2019-12-15 20:59:02 +0800

    0035 Week 02

---
+++ Week_01/G20190343020035/LeetCode_26_0035.java
@@ -0,0 +1,30 @@
+//给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，
+// 返回移除后数组的新长度。
+
+
+import org.junit.Test;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class LeetCode_26_0035 {
+    public int removeDuplicates(int[] nums) {
+        /**
+         *   定义一个快指针i ,慢指针 j
+         *   从数组头部开始遍历，如果2个元素相等，j++,否则 nums[i+1] = nums[j],i++,j++
+         */
+        int i = 0, j = 1;
+        for ( ; j < nums.length; ){
+            if(nums[i] == nums[j]){
+                j++;
+            }else{
+                nums[++i] = nums[j++];
+            }
+        }
+        return  i+1;
+    }
+
+    @Test
+    public void t1(){
+        System.out.println(removeDuplicates(new int[]{1,2,3}));
+    }
+}
+
---
+++ Week_01/G20190343020035/LeetCode_42_0035.java
@@ -0,0 +1,41 @@
+//给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，
+// 下雨之后能接多少雨水。
+
+// Related Topics 栈 数组 双指针
+
+//leetcode submit region begin(Prohibit modification and deletion)
+import org.junit.Test;
+
+import java.util.Stack;
+
+ public class LeetCode_42_0035 {
+
+    public int trap(int[] height) {
+        int allWaterArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        int current = 0;
+        while (current < height.length) {
+            //如果栈不空并且当前指向的高度大于栈顶高度就一直循环
+            while (!stack.empty() && height[current] > height[stack.peek()]) {
+                int h = height[stack.peek()]; //取出要出栈的元素
+                stack.pop();
+                if (stack.empty()) {
+                    break;
+                }
+                int distance = current - stack.peek() - 1;
+                int min = Math.min(height[stack.peek()], height[current]);
+                allWaterArea = allWaterArea + distance * (min - h);
+            }
+            stack.push(current);
+            current++;
+        }
+        return allWaterArea;
+
+    }
+
+    @Test
+    public void t1(){
+        trap(new int[]{1,2,3});
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
