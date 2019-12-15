3fff6db847b5034de11c975274882838c87a349f
Benny233 <1026658608@qq.com>
2019-12-15 18:08:55 +0800

    第一周作业

---
+++ Week_01/G20190343020009/1.两数之和.js
@@ -0,0 +1,48 @@
+// 第一版 小优化 两层for循环 时间复杂度nlogn
+const twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i,j]
+            }
+        }
+    }
+    return []
+}
+// 第二版 如何变单循环 map
+// 直接map.has就不用第二层循环了
+// 从循环查找 变成哈希查找 这应该算空间换时间
+// 能过 但是有问题 map的key不能重复，map.has只能找key
+const twoSum = function (nums, target) {
+    let amap = new Map()
+    // 先来一个循环？
+    for (let i = 0; i < nums.length; i++) {
+        amap.set(nums[i], i)
+    }
+    for (let i = 0; i < nums.length; i++) {
+        // 不能找自己
+        const wanted = target - nums[i]
+        if (amap.has(wanted)) {
+            if (i == amap.get(wanted)) {
+                continue
+            }
+            return [i, amap.get(wanted)]
+        }
+    }
+    return []
+}
+
+// 第三版 就是高票参考答案了
+const twoSum = function(nums, target) {
+    let amap = new Map()
+    let wanted
+    for (let i = 0; i < nums.length; i++) {
+        wanted = target - nums[i]
+        if (amap.has(wanted)) {
+            return [amap.get(wanted), i]
+        }
+        amap.set(nums[i], i)
+    }
+    return []
+}
+
---
+++ Week_01/G20190343020009/26.删除排序数组中的重复项.js
@@ -0,0 +1,24 @@
+/* 数组  双指针操作
+思路 
+整体上，是要把数组的后面往前面移动
+细节上，需要把重复的覆盖掉
+使用两个指针 
+慢指针i代表的是 已经整理好的部分
+快指针j遍历
+时间复杂度O(n) 空间复杂度O(1)
+*/
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) {
+        return 0
+    }
+    let i = 0
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] != nums[j]) {
+            nums[i + 1] = nums[j]
+            i++
+        }
+    }
+    return i + 1
+};
+
+
