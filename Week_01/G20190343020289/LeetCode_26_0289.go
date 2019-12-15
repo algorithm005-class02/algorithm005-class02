67e6c8b7d2b878bb8d49383484bb420688354789
李程俊 <183542728@qq.com>
2019-12-15 21:16:19 +0800

    0289_Week 01

--- Week_01/G20190343020289/LeetCode_26_0289.go
+++ Week_01/G20190343020289/LeetCode_26_0289.go
@@ -1,1 +1,17 @@
 package G20190343020289
+
+//26. 删除排序数组中的重复项
+// 快慢指针
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	j := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[j] {
+			j++
+			nums[j] = nums[i]
+		}
+	}
+	return j + 1
+}
--- Week_01/G20190343020289/LeetCode_641_0289.go
+++ Week_01/G20190343020289/LeetCode_641_0289.go
@@ -1,1 +1,94 @@
 package G20190343020289
+
+type MyCircularDeque struct {
+	data  []int
+	front int
+	last  int
+}
+
+/** Initialize your data structure here. Set the size of the deque to be k. */
+func Constructor(k int) MyCircularDeque {
+	return MyCircularDeque{
+		data:  make([]int, k+1, k+1),
+		front: 0,
+		last:  0,
+	}
+}
+
+/** Adds an item at the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertFront(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+
+	this.front = (len(this.data) + this.front - 1) % len(this.data)
+	this.data[this.front] = value
+	return true
+}
+
+/** Adds an item at the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertLast(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	this.data[this.last] = value
+	this.last = (this.last + 1) % len(this.data)
+	return true
+}
+
+/** Deletes an item from the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteFront() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	this.front = (this.front + 1) % len(this.data)
+	return true
+}
+
+/** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteLast() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	this.last = (len(this.data) + this.last - 1) % len(this.data)
+	return true
+}
+
+/** Get the front item from the deque. */
+func (this *MyCircularDeque) GetFront() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.data[this.front]
+}
+
+/** Get the last item from the deque. */
+func (this *MyCircularDeque) GetRear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.data[(len(this.data)+this.last-1)%len(this.data)]
+}
+
+/** Checks whether the circular deque is empty or not. */
+func (this *MyCircularDeque) IsEmpty() bool {
+	return this.last == this.front
+}
+
+/** Checks whether the circular deque is full or not. */
+func (this *MyCircularDeque) IsFull() bool {
+	return (this.last+1)%len(this.data) == this.front
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.InsertFront(value);
+ * param_2 := obj.InsertLast(value);
+ * param_3 := obj.DeleteFront();
+ * param_4 := obj.DeleteLast();
+ * param_5 := obj.GetFront();
+ * param_6 := obj.GetRear();
+ * param_7 := obj.IsEmpty();
+ * param_8 := obj.IsFull();
+ */
