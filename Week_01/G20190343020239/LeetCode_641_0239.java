21bef38145cdfd3669d4f991575925b41fa3e531
llkongl <854632129@qq.com>
2019-12-15 11:29:55 +0800

    0239-Week 01，心得

--- Week_01/G20190343020239/LeetCode_641_0239.java
+++ Week_01/G20190343020239/LeetCode_641_0239.java
@@ -8,9 +8,6 @@ class MyCircularDeque {
         public void setData(int data) {
             this.data = data;
         }
-        public void setNext(Node next) {
-            this.next = next;
-        }
     }
 
     private Node head;
