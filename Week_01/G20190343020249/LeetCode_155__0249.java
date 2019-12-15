5e19188f7112a2083b83402b722c74b7ed114ff4
CCzhiyun <550388087@qq.com>
2019-12-16 00:41:02 +0800

    Create LeetCode_155__0249.java

---
+++ Week_01/G20190343020249/LeetCode_155__0249.java
@@ -0,0 +1,17 @@
+package stack;/**
+ * author: Ziv
+ * description:
+ * Date:Created in
+ */
+
+/**
+ *
+ *
+ * @author piper
+ * @date 2019/12/11 23:45 
+ * @decrisption
+ */
+public class LeetCode_155__0249 {
+
+
+}
