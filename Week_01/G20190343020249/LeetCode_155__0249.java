de831f0218c75ca9f80ddb428c054af95883e371
CCzhiyun <550388087@qq.com>
2019-12-16 00:45:48 +0800

    Update LeetCode_155__0249.java

--- Week_01/G20190343020249/LeetCode_155__0249.java
+++ Week_01/G20190343020249/LeetCode_155__0249.java
@@ -12,6 +12,30 @@ package stack;/**
  * @decrisption
  */
 public class LeetCode_155__0249 {
+private Stack<Integer> stack;
+    private Stack<Integer> min_stack;
 
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        min_stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(min_stack.isEmpty() || x <= min_stack.peek())  min_stack.push(x);
+    }
+
+    public void pop() {
+        if(min_stack.peek().equals(stack.pop())) min_stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min_stack.peek();
+    }
 
 }
