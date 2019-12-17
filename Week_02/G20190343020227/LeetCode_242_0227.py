e12cc68699042990eb4d922d9b63fb16fcecdf87
max_chen <273206894@qq.com>
2019-12-17 14:09:48 +0800

    增加哈希表法

--- Week_02/G20190343020227/LeetCode_242_0227.py
+++ Week_02/G20190343020227/LeetCode_242_0227.py
@@ -1,4 +1,4 @@
-#一个非常偷懒的方法。。
+#一个非常偷懒的方法。。 48s
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -7,3 +7,30 @@ class Solution(object):
         :rtype: bool
         """
         return sorted(s) == sorted(t)
+
+
+#哈希表法 24s
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        d = {}
+        for i in s:
+            if i in d:
+                d[i] = d[i] + 1
+            else:
+                d[i] = 1
+        for i in t:
+            if i not in d:
+                return False
+            else:
+                d[i] = d[i] - 1
+        for i in d:
+            if d[i] != 0:
+                return False
+        return True
