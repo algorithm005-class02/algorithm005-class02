14be17281b4fa3149d9d7feae689348e739d4353
allo <alloevil@hotmail.com>
2019-12-17 20:36:35 +0800

    commit linkedList

---
+++ Week_01/G20190343020357/ReverseListLinkedList.java
@@ -0,0 +1,36 @@
+package com.array.reviewLinkedList;
+
+public class ReverseListLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode nextTemp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nextTemp;
+        }
+        return prev;
+    }
+
+    public ListNode reverseList1(ListNode head) {
+        ListNode newNode = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newNode;
+            newNode = head;
+            head = next;
+        }
+        return newNode;
+    }
+}
