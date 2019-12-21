c9932bd870251342020474c8878cc60c00ddc44f
star <imvmstar@gmail.com>
2019-12-21 16:32:37 +0800

    week 02

--- Week_02/G20190343020079/LeetCode_242_0079.java
+++ Week_02/G20190343020079/LeetCode_242_0079.java
@@ -11,7 +11,7 @@ public class Solution_3 {
     int[] counter = new int[26];
     for (int i = 0; i < s.length(); i++) {
       counter[s.charAt(i) - 'a'] ++;
-      counter[t.charAt(i) - 'a'] ++;
+      counter[t.charAt(i) - 'a'] --;
     }
 
     for (int count : counter) {
