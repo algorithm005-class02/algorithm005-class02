3d286566a2560abeb5287bae3eced083d00e1136
star <imvmstar@gmail.com>
2019-12-21 16:21:28 +0800

    week 02

---
+++ Week_02/G20190343020079/LeetCode_242_0079.java
@@ -0,0 +1,25 @@
+```
+package com.star.l242;
+
+/**
+ * @author star
+ * @Date 2019/12/21.
+ * @Description: No Description Yet.
+ */
+public class Solution_3 {
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) return false;
+    int[] counter = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      counter[s.charAt(i) - 'a'] ++;
+      counter[t.charAt(i) - 'a'] ++;
+    }
+
+    for (int count : counter) {
+      if (count != 0) return false;
+    }
+
+    return true;
+  }
+}
+```
---
+++ Week_02/G20190343020079/LeetCode_49_0079.java
@@ -0,0 +1,36 @@
+```
+package com.star.l49;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author star
+ * @Date 2019/12/21.
+ * @Description: No Description Yet.
+ * 字符串排序，用作key，value为原始字符串
+ */
+public class Solution_1 {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> res = new HashMap<>();
+    for (String s:strs) {
+      char[] charArr = s.toCharArray();
+      Arrays.sort(charArr);
+      String sortedStr = new String(charArr);
+
+      List<String> list = res.getOrDefault(sortedStr, new ArrayList<>());
+      list.add(s);
+      res.put(sortedStr, list);
+    }
+
+    List<List<String>> l = new ArrayList<>();
+    for (Map.Entry<String, List<String>> entry:res.entrySet()) {
+      l.add(entry.getValue());
+    }
+    return l;
+  }
+}
+```
