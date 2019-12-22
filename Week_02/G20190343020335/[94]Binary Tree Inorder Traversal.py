1134f5c8cf0dde8afcc78e2c73dba47edac1b134
pistoolster <lanlvdefan@gmail.com>
2019-12-22 22:34:53 +0800

    week 02 homework

---
+++ Week_02/G20190343020335/[94]Binary Tree Inorder Traversal.py
@@ -0,0 +1,39 @@
+#给定一个二叉树，返回它的中序 遍历。 
+#
+# 示例: 
+#
+# 输入: [1,null,2,3]
+#   1
+#    \
+#     2
+#    /
+#   3
+#
+#输出: [1,3,2] 
+#
+# 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+# Related Topics 栈 树 哈希表
+
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def __init__(self):
+        self.result = []
+
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root:
+            self.inorderTraversal(root.left)
+            self.result.append(root.val)
+            self.inorderTraversal(root.right)
+
+        return self.result
+        
+#leetcode submit region end(Prohibit modification and deletion)
