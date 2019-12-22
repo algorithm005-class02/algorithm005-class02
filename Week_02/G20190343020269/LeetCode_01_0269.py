b84a6e3bc66cec35ebedde77d39fabfd13036bcb
moi777 <29120566+moi777@users.noreply.github.com>
2019-12-22 13:23:23 +0800

    Add files via upload

---
+++ Week_02/G20190343020269/LeetCode_01_0269.py
@@ -0,0 +1,26 @@
+#给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+#
+# 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。 
+#
+# 示例: 
+#
+# 给定 nums = [2, 7, 11, 15], target = 9
+#
+#因为 nums[0] + nums[1] = 2 + 7 = 9
+#所以返回 [0, 1]
+# 
+# Related Topics 数组 哈希表
+
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def twoSum(self, nums, target):
+        hash = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in hash:
+                return[hash[target - nums[i]], i]
+            hash[nums[i]] = i
+        return[-1, -1]
+
+#leetcode submit region end(Prohibit modification and deletion)
