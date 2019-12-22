7da4c90c2e4c46916df5181a8d7904af87fb4781
Smart <726518972@qq.com>
2019-12-22 17:29:04 +0800

    week02

---
+++ Week_01/G20190343020115/com/smart33690/LeetCode094.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode094 {
+    public static void main(String[] args) {
+
+    }
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    public List < Integer > inorderTraversal(TreeNode root) {
+        List< Integer > res = new ArrayList< >();
+        helper(root, res);
+        return res;
+    }
+    public void helper(TreeNode root, List < Integer > res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
+
+}
---
+++ Week_01/G20190343020115/com/smart33690/LeetCode144.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LeetCode144 {
+    public static void main(String[] args) {
+
+    }
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return output;
+    }
+}
