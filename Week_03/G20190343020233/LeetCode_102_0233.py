c859add4719cd7b2f98b853d937a700d6e232394
Kernel <qilaidi@netease.com>
2019-12-29 14:51:28 +0800

    BinaryTreeLevelOrderTraversal_dfs

--- Week_03/G20190343020233/LeetCode_102_0233.py
+++ Week_03/G20190343020233/LeetCode_102_0233.py
@@ -7,3 +7,19 @@ class Solution:
             result_list.append([node.val for node in process_list])
             process_list = [item for pair in [(node.left, node.right) for node in process_list] for item in pair if item]
         return result_list
+
+    def levelOrder_dfs(self, root: TreeNode) -> List[List[int]]:
+        result_list = []
+
+        if not root:
+            return result_list
+        def generate_result(level, node):
+            if level == len(result_list):
+                result_list.append([])
+            if node:
+                result_list[level].append(node.val)
+            if node.left:
+                generate_result(level+1, node.left)
+            if node.right:
+                generate_result(level+1, node.right)
+        generate_result(0, root)
