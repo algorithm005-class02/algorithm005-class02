eb3de25b5b869f45ddb506c22e7620dfde0e44af
田雪峰 <tianxuefeng@shuidihuzhu.com>
2019-12-30 19:13:19 +0800

    105

---
+++ Week_02/G20190343020041/LeetCode_105_0041.py
@@ -0,0 +1,42 @@
+# 根据一棵树的前序遍历与中序遍历构造二叉树。
+#
+# 注意:
+# 你可以假设树中没有重复的元素。
+#
+# 例如，给出
+#
+# 前序遍历 preorder = [3,9,20,15,7]
+# 中序遍历 inorder = [9,3,15,20,7]
+#
+# 返回如下的二叉树：
+#
+#     3
+#   / \
+#  9  20
+#    /  \
+#   15   7
+# Related Topics 树 深度优先搜索 数组
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0:ind])
+            root.right = self.buildTree(preorder, inorder[ind + 1:])
+            return root
+
+# leetcode submit region end(Prohibit modification and deletion)
