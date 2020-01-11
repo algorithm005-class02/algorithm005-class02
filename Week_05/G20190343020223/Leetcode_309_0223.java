f2d8127228b445b71a8802edd28451c39d151b8e
jane <2829342005@qq.com>
2020-01-11 13:26:49 +0800

    Week 05

---
+++ Week_05/G20190343020223/Leetcode_309_0223.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=309 lang=java
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if(n==0)return 0;
+        
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
+            }
+            if (i == 1) {
+                dp[i][0] = Math.max(dp[0][0], dp[0][1] + prices[i]);
+                dp[i][1] = Math.max(dp[0][1], dp[0][0] - prices[i]);
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+}
+// @lc code=end
+
