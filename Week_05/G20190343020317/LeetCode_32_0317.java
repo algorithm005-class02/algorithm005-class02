a24e86aa51010bcce10faf43fab26d277ebf1fd5
liye <liye@zhongdihang.com>
2020-01-12 21:25:08 +0800

    第三周作业—0317

---
+++ Week_05/G20190343020317/LeetCode_32_0317.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int longestValidParentheses(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return 0;
+        }
+        int dp[] = new int[len];
+        int result = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                result = Math.max(result, dp[i]);
+            }
+        }
+        return result;
+    }
+}
---
+++ Week_05/G20190343020317/LeetCode_62_0317.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 1) return 1;
+        if (n == 1) return 1;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    result[i][j] = 1;
+                } else {
+                    result[i][j] = result[i - 1][j] + result[i][j - 1];
+                }
+            }
+        }
+        return result[m - 1][n - 1];
+    }
+}
