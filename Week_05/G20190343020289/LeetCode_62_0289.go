85b763a779939b17d6128b41c302c621b9914325
李程俊 <183542728@qq.com>
2020-01-12 11:24:18 +0800

    不同路径问题

--- Week_05/G20190343020289/LeetCode_62_0289.go
+++ Week_05/G20190343020289/LeetCode_62_0289.go
@@ -2,17 +2,30 @@ package G20190343020289
 
 func uniquePaths(m int, n int) int {
 	// 动态规划
+	//dp := make([][]int, m)
+	//for i := 0; i < m; i++ {
+	//	dp[i] = make([]int, n)
+	//	dp[i][0] = 1
+	//}
+	//for i := 0; i < n; i++ {
+	//	dp[0][i] = 1
+	//}
+	//for i := 1; i < m; i++ {
+	//	for j := 1; j < n; j++ {
+	//		dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	//	}
+	//}
+	//return dp[m-1][n-1]
+
 	dp := make([][]int, m)
 	for i := 0; i < m; i++ {
 		dp[i] = make([]int, n)
-		dp[i][0] = 1
-	}
-	for i := 0; i < n; i++ {
-		dp[0][i] = 1
-	}
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = 1
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
 		}
 	}
 	return dp[m-1][n-1]
--- Week_05/G20190343020289/LeetCode_63_0289.go
+++ Week_05/G20190343020289/LeetCode_63_0289.go
@@ -1,24 +1,41 @@
 package G20190343020289
 
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-	// temp
+	// 动态递推
 	m := len(obstacleGrid)
 	n := len(obstacleGrid[0])
+	if obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1 {
+		return 0
+	}
 	dp := make([][]int, m)
 	for i := 0; i < m; i++ {
 		dp[i] = make([]int, n)
-		if obstacleGrid[i][0] != 1 {
-			dp[i][0] = 1
-		}
-	}
-	for i := 0; i < n; i++ {
-		if obstacleGrid[0][i] != 1 {
-			dp[0][i] = 1
-		}
-	}
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				// finish位置
+				dp[i][j] = 1
+			} else if i == 0 {
+				// i为0的边界
+				if obstacleGrid[i][j] == 1 || dp[i][j-1] == 0 {
+					dp[i][j] = 0
+				} else {
+					dp[i][j] = 1
+				}
+			} else if j == 0 {
+				// j为0的边界
+				if obstacleGrid[i][j] == 1 || dp[i-1][j] == 0 {
+					dp[i][j] = 0
+				} else {
+					dp[i][j] = 1
+				}
+			} else {
+				// 非边界
+				if obstacleGrid[i][j] == 1 || (dp[i-1][j] == 0 && dp[i][j-1] == 0) {
+					dp[i][j] = 0
+				} else {
+					dp[i][j] = dp[i-1][j] + dp[i][j-1]
+				}
+			}
 		}
 	}
 	return dp[m-1][n-1]
