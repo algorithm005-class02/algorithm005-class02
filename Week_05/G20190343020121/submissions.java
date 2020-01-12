9f6b8bebc71670b16de85d4eeb29b1682c0f93aa
slwang613928 <865499908@qq.com>
2020-01-12 19:04:17 +0800

    第五周作业
    第五周作业

---
+++ Week_05/G20190343020121/submissions.java
@@ -0,0 +1,26 @@
+class Solution {
+   public boolean canCross(int[] stones) {
+        int[][] memo = new int[stones.length][stones.length];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return can_Cross(stones, 0, 0, memo) == 1;
+    }
+    public int can_Cross(int[] stones, int ind, int jumpsize, int[][] memo) {
+        if (memo[ind][jumpsize] >= 0) {
+            return memo[ind][jumpsize];
+        }
+        for (int i = ind + 1; i < stones.length; i++) {
+            int gap = stones[i] - stones[ind];
+            if (gap >= jumpsize - 1 && gap <= jumpsize + 1) {
+                if (can_Cross(stones, i, gap, memo) == 1) {
+                    memo[ind][gap] = 1;
+                    return 1;
+                }
+            }
+        }
+        memo[ind][jumpsize] = (ind == stones.length - 1) ? 1 : 0;
+        return memo[ind][jumpsize];
+    }
+
+}
