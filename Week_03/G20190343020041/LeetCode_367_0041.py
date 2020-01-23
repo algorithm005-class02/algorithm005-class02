5e0312be60924d422857f5ef365976a4edc36b89
田雪峰 <tianxuefeng@shuidihuzhu.com>
2020-01-23 19:15:00 +0800

    367

--- Week_03/G20190343020041/LeetCode_367_0041.py
+++ Week_03/G20190343020041/LeetCode_367_0041.py
@@ -1,34 +1,36 @@
 # 给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
 #
-# 说明：不要使用任何内置的库函数，如 sqrt。
+#  说明：不要使用任何内置的库函数，如 sqrt。
 #
-# 示例 1：
+#  示例 1：
 #
-# 输入：16
+#  输入：16
 # 输出：True
 #
-# 示例 2：
+#  示例 2：
 #
-# 输入：14
+#  输入：14
 # 输出：False
 #
-# Related Topics 数学 二分查找
+#  Related Topics 数学 二分查找
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        if num < 2:
-            return True
-
+        if num < 2: return True
         left, right = 2, num // 2
         while left <= right:
             mid = left + (right - left) // 2
-            if mid * mid == num:
+            s = mid * mid
+            if s == num:
                 return True
-            elif mid * mid < num:
-                left = mid - 1
+            if s > num:
+                right = mid - 1
             else:
-                right = mid + 1
+                left = mid + 1
         return False
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+Solution().isPerfectSquare(8)
