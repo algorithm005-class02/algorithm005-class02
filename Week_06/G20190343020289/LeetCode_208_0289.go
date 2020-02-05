bb2258583dff74ae3f8a8d5483a458ac6236aa54
李程俊 <183542728@qq.com>
2020-02-05 20:57:11 +0800

    trie tree2

--- Week_06/G20190343020289/LeetCode_208_0289.go
+++ Week_06/G20190343020289/LeetCode_208_0289.go
@@ -2,14 +2,13 @@ package G20190343020289
 
 // 208. 实现 Trie (前缀树)
 type Trie struct {
-	val      byte
 	children [26]*Trie
 	isEnd    bool
 }
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-	return Trie{}
+	return Trie{isEnd: true}
 }
 
 /** Inserts a word into the trie. */
@@ -18,44 +17,38 @@ func (this *Trie) Insert(word string) {
 	for i := 0; i < len(word); i++ {
 		charIndex := int(word[i]) - int('a')
 		if root.children[charIndex] == nil {
-			var zz Trie
-			zz.val = word[i]
-			root.children[charIndex] = &zz
+			root.children[charIndex] = new(Trie)
+			root.children[charIndex].isEnd = false
 		}
 		root = root.children[charIndex]
-		if i == len(word)-1 {
-			root.isEnd = true
-		}
 	}
+	root.isEnd = true
 }
 
 /** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-	root := this
-	for i := 0; i < len(word); i++ {
-		charIndex := int(word[i]) - int('a')
-		if root.children[charIndex] == nil {
-			return false
-		}
-		root = root.children[charIndex]
-		if i == len(word)-1 && !root.isEnd {
-			return false
-		}
-	}
-	return true
+	return this.search(word, 1)
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-	root := this
-	for i := 0; i < len(prefix); i++ {
-		charIndex := int(prefix[i]) - int('a')
-		if root.children[charIndex] == nil {
+	return this.search(prefix, 2)
+}
+
+func (root *Trie) search(str string, t int) bool {
+	current := root
+	for i := 0; i < len(str); i++ {
+		charIndex := int(str[i]) - int('a')
+		if current.children[charIndex] == nil {
 			return false
 		}
-		root = root.children[charIndex]
+		current = current.children[charIndex]
 	}
-	return true
+	if t == 1 {
+		return current.isEnd
+	} else {
+		return true
+	}
 }
 
 /**
