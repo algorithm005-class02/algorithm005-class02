a19267816e7e22ed7285dee16cfc3d37d2761a21
int86 <273206894@qq.com>
2020-02-05 19:17:35 +0800

    位1的个数

--- Week_07/G20190343020227/LeetCode_190_0227.go
+++ Week_07/G20190343020227/LeetCode_190_0227.go
@@ -1,8 +1,8 @@
 func reverseBits(num uint32) uint32 {
     var res uint32
     for i:=0;i<32;i++{
-        res = (res<<1)+(num&1)
-        num = num >>1
+        res = res | (((num>>(31-i))&1)<<i)
+        
     }
     return res
 }
