d9e4767651b9b76038e0b99155befffb3a6cb638
qilaidi <qilaidi@vip.qq.com>
2020-02-07 11:51:55 +0800

    190ReverseBits

--- Week_07/G20190343020233/LeetCode_190_0233.py
+++ Week_07/G20190343020233/LeetCode_190_0233.py
@@ -2,10 +2,22 @@
 # Create on 2020/2/6
 
 class Solution:
-    def reverseBits(self, n):
+    def reverseBits_str(self, n):
+        """28 ms"""
+        """超哥说这是脱了裤子放屁的办法"""
         return int(str(bin(n))[2:].zfill(32)[::-1], 2)
+
+    def reverseBits(self, n):
+        """52 ms, 但为什么直接位运算还比较慢呢"""
+        res = 0
+        for _ in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        return res
+
+
 
 if __name__ == '__main__':
     test = Solution()
-    print(test.reverseBits(0b00000010100101000001111010011100)) #00111001011110000010100101000000
-    print(test.reverseBits(0b11111111111111111111111111111101)) #10111111111111111111111111111111
+    print(test.reverseBits(0b00000010100101000001111010011100)) #964176192
+    print(test.reverseBits(0b11111111111111111111111111111101)) #3221225471
