cde65b9de8ecd5c255b70babef34b38e7f6cc6da
gaoruilin <gaoruilin@kuandeng.com>
2020-02-09 21:00:10 +0800

    leetCode commit

---
+++ Week_07/G20190343020357/HammingWeight.java
@@ -0,0 +1,17 @@
+package week07;
+
+public class HammingWeight {
+
+    public int hammingWeight(int n) {
+        int bits = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask <<= 1;
+        }
+        return bits;
+    }
+
+}
