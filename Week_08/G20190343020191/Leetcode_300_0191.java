76bc4182a3c3c024161aeca88be8f1a41e8fbf12
rateyu <rateyu@gmail.com>
2020-02-16 19:32:31 +0800

    第八周作业提交

---
+++ Week_08/G20190343020191/Leetcode_300_0191.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int memo[][] = new int[nums.length + 1][nums.length];
+        for (int[] l : memo) {
+            Arrays.fill(l, -1);
+        }
+        return lengthofLIS(nums, -1, 0, memo);
+    }
+    public int lengthofLIS(int[] nums, int previndex, int curpos, int[][] memo) {
+        if (curpos == nums.length) {
+            return 0;
+        }
+        if (memo[previndex + 1][curpos] >= 0) {
+            return memo[previndex + 1][curpos];
+        }
+        int taken = 0;
+        if (previndex < 0 || nums[curpos] > nums[previndex]) {
+            taken = 1 + lengthofLIS(nums, curpos, curpos + 1, memo);
+        }
+
+        int nottaken = lengthofLIS(nums, previndex, curpos + 1, memo);
+        memo[previndex + 1][curpos] = Math.max(taken, nottaken);
+        return memo[previndex + 1][curpos];
+    }
+}
