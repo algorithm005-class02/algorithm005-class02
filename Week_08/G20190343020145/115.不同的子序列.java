d7cebfcedaa2accaa60ff8392f5b9b8aa3072f1f
leowind0731 <leowind0731@126.com>
2020-02-16 23:01:01 +0800

    0145_Week08

---
+++ Week_08/G20190343020145/115.不同的子序列.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[t.length() + 1][s.length() + 1];
+        for(int i = 0; i < s.length() + 1; ++ i){
+            dp[0][i] = 1;
+        }
+        for(int i = 1; i < t.length() + 1; ++ i){
+            for(int j = 1; j < s.length() + 1; ++j){
+                if(t.charAt(i -1) == s.charAt(j -1)){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                }else{
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[t.length()][s.length()];
+        
+    }
+}
