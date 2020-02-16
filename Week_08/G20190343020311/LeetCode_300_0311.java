a918527a1c32c48be4e8f5b6671be3f81f2dda19
Qifeng Zheng <plus@plusnow.me>
2020-02-17 01:42:37 +0800

    第八周作业

---
+++ Week_08/G20190343020311/LeetCode_300_0311.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for(int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+
+        return len;
+    }
+}
