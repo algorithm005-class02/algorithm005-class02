527b233e2b9896203fa0b74439d336a74300c5a9
oldrain <oldraindev+github@gmail.com>
2020-02-16 22:25:24 +0800

    0025_Week 07 && 08

---
+++ Week_07/G20190343020259/LeetCode_190_0259.go
@@ -0,0 +1,11 @@
+package G20190343020259
+
+func reverseBits(num uint32) uint32 {
+	var res uint32 = 0
+	for i := 0; i < 32; i++ {
+		res <<= 1
+		res |= num & 1
+		num >>= 1
+	}
+	return res
+}
---
+++ Week_08/G20190343020259/LeetCode_205_0259.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null || t == null) return false;
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> mapS = new HashMap<Character, Integer>();
+        Map<Character, Integer> mapT = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            int indexS = mapS.getOrDefault(s.charAt(i), -1);
+            int indexT = mapT.getOrDefault(t.charAt(i), -1);
+
+            if (indexS != indexT) {
+                return false;
+            }
+
+            mapS.put(s.charAt(i), i);
+            mapT.put(t.charAt(i), i);
+        }
+
+        return true;
+    }
+}
