aadfad90cce690ed8a0017db292c4f11365984af
lilin233 <1002351468@qq.com>
2020-02-23 19:04:05 +0800

    week6

---
+++ Week_06/G20190343020271/FriendCycle.h
@@ -0,0 +1,17 @@
+//
+//  FriendCycle.h
+//  Algorithm
+//
+//  Created by liuk on 2020/2/23.
+//  Copyright © 2020 glodon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FriendCycle : NSObject
+int findCircleNum(int*_Nullable* M, int MSize, int* MColSize);
+@end
+
+NS_ASSUME_NONNULL_END
